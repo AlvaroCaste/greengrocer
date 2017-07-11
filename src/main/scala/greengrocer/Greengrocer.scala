48774d2efbb888c5e7c7e2cba56f7881d27ddc89
Álvaro Castellanos <alvaro.castellanos.lopez@gmail.com>
2017-07-11 14:17:12 +0200

    Change Price name to Money

--- src/main/scala/greengrocer/Greengrocer.scala
+++ src/main/scala/greengrocer/Greengrocer.scala
@@ -4,14 +4,14 @@ import greengrocer.Greengrocer.Item.{Apple, Orange}
 
 object Greengrocer extends App {
 
-  case class Price(price: Int) {
-    def +(price: Price) = Price(this.price + price.price)
+  case class Money(amount: Int) {
+    def +(money: Money) = Money(this.amount + money.amount)
 
     override def toString = {
-      if (price < 100) s"${price}p"
+      if (amount < 100) s"${amount}p"
       else {
-        val integerPart = price / 100
-        val decimalPart = price % 100
+        val integerPart = amount / 100
+        val decimalPart = amount % 100
         decimalPart match {
           case zero if zero == 0 => s"£$integerPart"
           case lessThan if lessThan < 10 => s"£$integerPart.0$decimalPart"
@@ -23,11 +23,11 @@ object Greengrocer extends App {
 
   sealed trait Item {
     val name: String
-    val price: Price
+    val price: Money
   }
   object Item {
-    case class Orange(name: String = "Orange", price: Price = Price(25)) extends Item
-    case class Apple(name: String = "Apple", price: Price = Price(60)) extends Item
+    case class Orange(name: String = "Orange", price: Money = Money(25)) extends Item
+    case class Apple(name: String = "Apple", price: Money = Money(60)) extends Item
   }
 
   def getTotalPrice(fruit: String*): String = fruit.map {
--- src/test/scala/greengrocer/MoneyTest.scala
+++ src/test/scala/greengrocer/MoneyTest.scala
@@ -1,29 +1,29 @@
 package greengrocer
 
 import org.scalatest.{FlatSpec, Matchers}
-import greengrocer.Greengrocer.Price
+import greengrocer.Greengrocer.Money
 
-class PriceTest extends FlatSpec with Matchers {
+class MoneyTest extends FlatSpec with Matchers {
 
-  behavior of "Price"
+  behavior of "Money"
 
   it should "show pence when is less than 100" in {
-    val price = Price(99)
+    val price = Money(99)
     price.toString shouldBe "99p"
   }
 
   it should "show decimal part when there is" in {
-    val price = Price(115)
+    val price = Money(115)
     price.toString shouldBe "£1.15"
   }
 
   it should "show one pound when is 100" in {
-    val price = Price(100)
+    val price = Money(100)
     price.toString shouldBe "£1"
   }
 
   it should "show decimal part with 0 when is less than 10" in {
-    val price = Price(105)
+    val price = Money(105)
     price.toString shouldBe "£1.05"
   }
 }
